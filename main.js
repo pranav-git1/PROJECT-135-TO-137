5a57d430e493fa10a6e8b3512141181778320c49
pranav-git1 <77086184+pranav-git1@users.noreply.github.com>
2021-08-20 18:51:03 +0530

    uploaded

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+objects = [];
 status = "";
 input = "";
 
@@ -10,16 +11,40 @@ function setup() {
 }
 
 function draw() {
-    image(video, 0, 0, 300, 300);
+    image(video, 0, 0, 350, 350);
+    if (status != "") {
+        objectDetector.detect(video, gotResult);
+        for (i = 0; i < objects.length; i++) {
+            percent = floor(objects[i].confidence * 100);
+            if (objects[i].label == input) {
+                video.stop();
+                objectDetector.detect(gotResult);
+                document.getElementById("status").innerHTML = "Status: OBJECTS DETECTED";
+                var synth = window.speechSynthesis;
+                speak_data = objects[i].label;
+                var utterThis = new SpeechSynthesisUtterance(speak_data);
+                synth.speak(utterThis);
+            }
+        }
+    }
 }
 
 function start() {
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
     document.getElementById("status").innerHTML = "Status: DETECTING OBJECTS";
-    input = document.getElementById("input_object").nodeValue;
+    input = document.getElementById("input_object").value;
 }
 
 function modelLoaded() {
     console.log("modelLoaded");
     status = true;
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        objects = results;
+    }
 }
